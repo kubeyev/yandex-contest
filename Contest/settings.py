f9f46cb3273c74c95330554e95a807a70af2c0d2
Abylay <kubeyevabl@gmail.com>
2021-03-28 18:07:46 +0600

    Allowed hosts changed

--- Contest/settings.py
+++ Contest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '30*0d+^^!2dg^uzu2nus^xdn5l%d4ils&2c5hp+xfp^mrwudf@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
