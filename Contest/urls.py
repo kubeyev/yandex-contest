92f8fb87a33479bee96e903f12f2d08e8bf1b60a
Abylay <kubeyevabl@gmail.com>
2021-03-28 18:58:49 +0600

    Static files configuration added to settings.py

--- Contest/urls.py
+++ Contest/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('couriers', include('couriers.urls')),
                   path('orders', include('orders.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + staticfiles_urlpatterns
